e49b92c2c40875ff2487ed4f0f134e664f157a67
skrBang <s_huabang@163.com>
2020-02-22 00:18:06 +0800

    Revert "something changes"
    This reverts commit 0e08c4a6

--- app/__init__.py
+++ app/__init__.py
@@ -55,10 +55,10 @@ def create_app(config_class=Config):
     app.register_blueprint(errors_bp)  # 注册错误处理blueprint
 
     from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(auth_bp, url_prefix='/auth')  # 注册用户认证blueprint
 
     from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+    app.register_blueprint(main_bp)  # 注册主业务blueprint
 
     # elasticsearch 属性
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
